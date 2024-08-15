0fff591283f1e3110c2846a83640f7f65de3208a
oidiego <163161590+oidiego@users.noreply.github.com>
2024-08-15 13:23:34 -0300

    finalizando aula 6
    terminei de configurar as animações.

--- oficinadejogos/Assets/scripts/player.cs
+++ oficinadejogos/Assets/scripts/player.cs
@@ -14,11 +14,14 @@ public class player : MonoBehaviour
     public bool isJumping;
 
     public bool doublejump;
+
+    private Animator Anim;
     
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,6 +36,23 @@ public class player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f,0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            Anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            Anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+        
+        if (Input.GetAxis("Horizontal") == 0f)
+        {
+            Anim.SetBool("walk", false);
+        }
     }
 
     void Jump()
@@ -43,6 +63,7 @@ public class player : MonoBehaviour
             {
                 rig.AddForce(new Vector2(0f,JumpForce), ForceMode2D.Impulse);
                 doublejump = true;
+                Anim.SetBool("jump", true);
             }
             else
             {
@@ -62,6 +83,7 @@ public class player : MonoBehaviour
         if (collision.gameObject.layer == 8)
         {
             isJumping = false;
+            Anim.SetBool("jump", false);
         }
     }
 
