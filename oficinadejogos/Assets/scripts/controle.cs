2eed7fcf69446f3d5a81452d4ef921098a2dff29
oidiego <163161590+oidiego@users.noreply.github.com>
2024-08-20 16:18:37 -0300

    aula 8 concluida

--- oficinadejogos/Assets/scripts/controle.cs
+++ oficinadejogos/Assets/scripts/controle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI; 
 
@@ -7,7 +8,7 @@ public class controle : MonoBehaviour
 {
     public int totalScore;
     public static controle instance;
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
     
     // Start is called before the first frame update
     void Start()
